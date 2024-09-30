6119b78aeab04910fa34ca4c03ad6d1b7e0a82f2
revanthkunche1206 <155340253+revanthkunche1206@users.noreply.github.com>
2024-09-30 15:45:32 +0530

    DoublyLinkedList.py

--- Task3-DoublyLinkedList/Doubly Linked List Implementation code.md
+++
@@ -1,9 +0,0 @@
-![image](https://github.com/user-attachments/assets/6dccb03a-db17-45f8-b84b-dd71f20e53d0)
-
-![image](https://github.com/user-attachments/assets/3cf91375-ef3a-448d-9976-53124eb54ae0)
-
-![image](https://github.com/user-attachments/assets/f6c9dca8-ceed-4f0d-b5d8-c400dd7b2b50)
-
-![image](https://github.com/user-attachments/assets/a1e4b395-3f24-486a-a38f-13ad15e8f232)
-
-![image](https://github.com/user-attachments/assets/adfcbf1f-15eb-499d-855e-796b2e965465)
---
+++ Task3-DoublyLinkedList/DoublyLinkedList.py
@@ -0,0 +1,125 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_beginning(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def insert_ending(self, data):
+        new_node = Node(data)
+        if self.tail is None:
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def insert_at_specificnode(self, node, data):
+        if node <= 0:
+            print("Invalid position!")
+            return
+        new_node = Node(data)
+        current = self.head
+        i = 1
+
+        while current is not None and i < node:
+            current = current.next
+            i += 1
+        
+        if current is None: 
+            print("Position exceeds the list length!")
+        else:
+            new_node.next = current
+            new_node.prev = current.prev
+            if current.prev:
+                current.prev.next = new_node
+            current.prev = new_node
+
+            if current == self.head:
+                self.head = new_node 
+
+    def delete_at_beginning(self):
+        if self.head is None:
+            print("Empty list")
+        elif self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+    def delete_at_end(self):
+        if self.tail is None:
+            print("Empty list")
+        elif self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+
+    def delete_at_specificnode(self, node):
+        if node <= 0:
+            print("Invalid position!")
+            return
+        current = self.head
+        i = 1
+        while current and i < node:
+            current = current.next
+            i += 1
+
+        if current is None: 
+            print("Position exceeds the list length!")
+        else:
+            if current.prev:
+                current.prev.next = current.next
+            if current.next:
+                current.next.prev = current.prev
+
+            if current == self.head:
+                self.head = current.next 
+            if current == self.tail:
+                self.tail = current.prev 
+    def traversal_forward(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+        print()
+
+    def traversal_backward(self):
+        current = self.tail
+        while current:
+            print(current.data, end=" ")
+            current = current.prev
+        print()
+
+DLL = DoublyLinkedList()
+
+DLL.insert_beginning(5)
+DLL.insert_beginning(4)
+DLL.insert_beginning(3)
+DLL.insert_beginning(2)
+DLL.insert_beginning(1)
+
+DLL.insert_beginning(10)
+DLL.insert_ending(15)
+
+DLL.insert_at_specificnode(3, 30)
+
+DLL.delete_at_specificnode(5)
+
+DLL.traversal_backward()
+DLL.traversal_forward()
+
